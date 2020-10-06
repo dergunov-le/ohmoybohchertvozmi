0f79943546a6a43a442d55a6b8ad2321d843422b
dergunov-le <lev-dergunov@mail.ru>
2020-10-06 16:30:30 +0900

    aufka

--- main.py
+++ main.py
@@ -5,5 +5,20 @@ print(9 / 4) #Деление с остатком
 print(9 // 4) #Деление без остатка
 name = 10
 print(name)
+print(1)
+print('1')
 i = 'я харашо пакакакакакал'
 print(i)
+n = '666'
+m = 228
+print(int(n) + m)
+print(n + str(m))
+print(f'{n}{m}')
+j = f'{m} - "это переменная"'
+print(j)
+print(n, m)
+print(n, f'\n{m}')
+
+name1 = int(input('Введите число '))
+name2 = int(input('Введите число '))
+print(name1 + name2)
